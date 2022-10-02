46ad81627087449cd937caa83b67455a53773107
prljav <74116121+prljav@users.noreply.github.com>
2022-10-02 13:25:16 +0200

    basically done i hope?

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Script for decompiling pyinstaller executables
 # ALL CREDIT GOES TO extremecoders-re
 https://github.com/extremecoders-re/pyinstxtractor<br>
 and<br>
-https://github.com/rocky/python-uncompyle6/
+https://github.com/rocky/python-uncompyle6/<br>
+
+```
+$ bash <(curl -s https://raw.githubusercontent.com/prljav/decompyle-python/main/install.sh)
+```
---
+++ install.sh
@@ -0,0 +1,17 @@
+function confirm {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;
+            [Nn]*) return 1  ;;
+        esac
+    done
+}
+
+confirm && echo "Download pyinstxtractor in current folder and install python-uncompyle6?" || exit 1
+wget https://raw.githubusercontent.com/extremecoders-re/pyinstxtractor/master/pyinstxtractor.py ~
+mkdir /tmp/prljavsetup
+cd /tmp/prljavsetup
+wget https://github.com/rocky/python-uncompyle6/archive/refs/heads/master.zip &&t unzip master.zip
+cd python-uncompyle6-master
+python3 setup.py install
